5d82bdafc4cdcd54e82c8365cac5281567b505a4
Lavedonio <daniel.lavedonio@gmail.com>
2019-06-26 05:14:22 -0300

    Main atualizada para novas funcionalidades implementadas.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from .inicializar import *
 from .interface import *
 from .compilar import *
+from .loader import *
 
 import os
 
 
 def main():
     run = True
+
+    # Inicializando memória
+    Memoria = Inicializar_Memoria_Virtual()
+
+    # Carregando instruções disponíveis
+    instr_set_1, rev_instr_set_1 = load_available_instructions("instruction_set_dig1.txt", multi_col=True)
+    instr_set_2, rev_instr_set_2 = load_available_instructions("instruction_set_dig2.txt")
 
     while run:
         opcao = menu()
@@ -15,17 +23,17 @@ def main():
         if opcao == "1":
             os.makedirs(os.path.join(os.getcwd(), "Arquivos_ASM"), exist_ok=True)
             arquivo = instrucoes_compilar()
-            instr_set_1 = load_available_instructions("instruction_set_dig1.txt")
-            instr_set_2 = load_available_instructions("instruction_set_dig2.txt")
-            compile(arquivo, instr_set_1, instr_set_2)
+            marcadores = encontrar_marcadores(arquivo, instr_set_1)
+            compile(arquivo, marcadores, instr_set_1, instr_set_2)
 
         # Load
         elif opcao == "2":
-            pass
+            arquivo = instrucoes_loader()
+            load(Memoria, arquivo)
 
         # Memória
         elif opcao == "3":
-            pass
+            Memoria.mostrar()
 
         # Run
         elif opcao == "4":
